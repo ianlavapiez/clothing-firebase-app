fc536d311f7a62fb8402a0bf71e172bba98dd52c
ianlavapiez <ianlavapiez@gmail.com>
2019-12-22 22:00:28 +0800

    updated collections

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       {/* Switch catches the first path and then not renders the rest */}
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
