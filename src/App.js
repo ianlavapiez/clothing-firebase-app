d0df59126dd7b87c4b9174aab9d08f0dd21684cb
ianlavapiez <ianlavapiez@gmail.com>
2019-12-22 21:35:32 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -6,7 +8,11 @@ import HomePage from "./pages/homepage/homepage.component";
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch catches the first path and then not renders the rest */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" />
+      </Switch>
     </div>
   );
 }
