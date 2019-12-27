fecfad0058e0b707d613ca448d6219546e07fec9
ianlavapiez <ianlavapiez@gmail.com>
2019-12-27 15:44:46 +0800

    added redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
-import "./index.css";
-import App from "./App";
+import './index.css'
+import App from './App'
+
+import store from './redux/store'
 
 ReactDOM.render(
-  // BrowserRouter gives our app all of the functionality of routing
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+  // Provider allows us to access the store in our application
+  <Provider store={store}>
+    {/* BrowserRouter gives our app all of the functionality of routing */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
