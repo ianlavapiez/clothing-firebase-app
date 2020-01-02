df253df80c9a867ada1de50c27c5e3498de34faa
ianlavapiez <ianlavapiez@gmail.com>
2020-01-02 21:55:43 +0800

    added new directory redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
 
 import rootReducer from './root.reducer'
 
 const middlewares = [logger]
 // applyMiddleware allows us to use multiple middlewares based from the constant middleware array list
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
-
-export default store
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const persistor = persistStore(store)
