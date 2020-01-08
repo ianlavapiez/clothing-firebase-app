d8695a4b14f93f5fd27d3d872c87db6bbf05fade
ianlavapiez <ianlavapiez@gmail.com>
2020-01-08 14:16:07 +0800

    fixed from container pattern

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 import rootReducer from './root.reducer'
 
-const middlewares = [logger]
+const middlewares = [thunk, logger]
 // applyMiddleware allows us to use multiple middlewares based from the constant middleware array list
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export const persistor = persistStore(store)
